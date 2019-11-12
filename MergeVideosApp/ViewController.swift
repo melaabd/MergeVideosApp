9b3a2b2fe0da075ffd4e90c899f0b0f4fef36c12
Mohamad El-Abd <mohamd.elaabd@gmail.com>
2019-11-12 11:13:10 +0400

    Initial Commit

---
+++ MergeVideosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MergeVideosApp
+//
+//  Created by Alfoah IOS on 12/11/2019.
+//  Copyright © 2019 El-Abd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
